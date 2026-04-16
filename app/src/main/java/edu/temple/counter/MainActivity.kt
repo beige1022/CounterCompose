1382c99f1b1e78ecb5bd14a4a93df3b0061dd82d
beige <markoguaman45@gmail.com>
2026-04-16 12:22:53 -0400

    Completed with minor unresolved reference error in value

---
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -0,0 +1,77 @@
+package edu.temple.counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.temple.counter.ui.theme.CounterTheme
+import  androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.painterResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Counter (modifier: Modifier = Modifier) {
+
+    var counterVal = remember {mutableSetOf(0)}
+
+    Column(modifier = modifier.fillMaxSize(),verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick =  {
+            counterVal.value++
+        }) { Image(painterResource(android.R.drawable.arrow_up_float), contentDescription = "Up Arrow") }
+        Text(
+            text = counterVal.value.toString(),
+            fontSize = 72.sp,
+            modifier = modifier
+        )
+        Button(onClick = {
+            if (counterVal.value > 0)
+                counterVal.value--
+        }) {Image(painterResource(android.R.drawable.arrow_down_float), contentDescription = "Down Arrow") }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CounterTheme {
+        Greeting("Android")
+    }
+}
